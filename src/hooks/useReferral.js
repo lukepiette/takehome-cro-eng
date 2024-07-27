9382bf8a689cf11794be1d8866b868c6da884858
pardeep <ps0296@gmail.com>
2024-07-27 19:09:38 -0400

    feat: test performance

--- src/hooks/useReferral.js
+++ src/hooks/useReferral.js
@@ -19,17 +19,16 @@ export default function useReferral() {
       const referralId = window.localStorage.getItem("runpodReferralId");
       setRefId(referralId);
 
-      // if (process.env.NEXT_PUBLIC_DEV && referralId !== "runpod")
-      //   window.location.replace(
-      //     "https://www.runpod.io" + window.location.pathname
-      //   );
+      if (process.env.NEXT_PUBLIC_DEV && referralId !== "runpod")
+        window.location.replace(
+          "https://www.runpod.io" + window.location.pathname
+        );
     }
   }, [router]);
 
-  return true;
-  // if (process.env.NEXT_PUBLIC_DEV && refId !== "runpod") {
-  //   return false;
-  // } else {
-  //   return true;
-  // }
+  if (process.env.NEXT_PUBLIC_DEV && refId !== "runpod") {
+    return false;
+  } else {
+    return true;
+  }
 }
