a25e94c150e7bf76a671d5eaee530418d08ec476
Jacob Wright <36747872+jacobcwright@users.noreply.github.com>
2024-08-15 11:32:20 -0600

    feat: add internal ref link (#111)

--- src/hooks/useReferrer.ts
+++ src/hooks/useReferrer.ts
@@ -1,22 +1,22 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-const organicReferrers = [
-  'https://google.com',
-  'https://bing.com',
-  'https://yahoo.com',
-  'https://duckduckgo.com',
-]
-
 export default function useReferrer() {
   const router = useRouter()
   const [referrer, setReferrer] = useState('')
 
   useEffect(() => {
-    if (typeof window === 'undefined') return
+    if (typeof window === 'undefined' || !router.isReady) return
 
     setReferrer(document.referrer)
-    window.localStorage.setItem('referrer_link', referrer)
+    if (
+      referrer.includes('blog.runpod.io') ||
+      referrer.includes('docs.runpod.io')
+    ) {
+      window.localStorage.setItem('internal_referrer_link', referrer)
+    } else {
+      window.localStorage.setItem('referrer_link', referrer)
+    }
   }, [router.isReady])
 
   return referrer
