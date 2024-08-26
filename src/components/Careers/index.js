66a41bf0116496158ff3d346986a9540a72c03c0
Jacob Wright <36747872+jacobcwright@users.noreply.github.com>
2024-08-26 08:24:41 -0600

    fix: swap jobs board to greenhouse (#113)
    * fix: swap jobs board to greenhouse
    
    * fix: career's page grammar

--- src/components/Careers/index.js
+++ src/components/Careers/index.js
@@ -1,57 +1,104 @@
-import { Box, Stack, Typography, useTheme, Button } from "@mui/material"
+import { Box, Stack, Typography, useTheme, Button } from '@mui/material'
 
 export default function Careers() {
   const theme = useTheme()
 
   return (
     <>
-      <Stack alignItems={"center"} mb={20} mt={8} position="relative">
+      <Stack alignItems={'center'} mb={20} mt={8} position='relative'>
         <Box
           sx={{
-            backgroundImage: "url(/static/images/bg.webp)",
-            backgroundRepeat: "no-repeat",
-            filter: "blur(60px)",
+            backgroundImage: 'url(/static/images/bg.webp)',
+            backgroundRepeat: 'no-repeat',
+            filter: 'blur(60px)',
             height: 1000,
-            overflowX: "hidden !important",
-            position: "absolute",
+            overflowX: 'hidden !important',
+            position: 'absolute',
             top: -400,
-            transform: "rotate(130deg)",
+            transform: 'rotate(130deg)',
             width: 900,
             zIndex: -1,
           }}
         />
 
-        <Typography align={"center"} mb={4} variant="h1">
+        <Typography align={'center'} mb={4} variant='h1'>
           Careers at RunPod
         </Typography>
-        <Stack alignItems="left" spacing={2} sx={{ maxWidth: 700, px: 2 }}>
-          <Typography align="left" fontSize={18}>
-            <b>All of our roles are either hybrid or remote.</b> Most of our team is based in SF and New Jersey. We are currently looking to grow our SF team, and have a preference for candidates who are enthusiastic about working in-person.
+        <Stack alignItems='left' spacing={2} sx={{ maxWidth: 700, px: 2 }}>
+          <Typography align='left' fontSize={18}>
+            <b>All of our roles are either hybrid or remote.</b> Most of our
+            team is based in SF and New Jersey. We are currently looking to grow
+            our SF team, and have a preference for candidates who are
+            enthusiastic about working in-person.
           </Typography>
-          <Typography align="left" fontSize={18}>
-            <b>Everyone on our team wears multiple hats.</b> Our sales team is has a strong technical background. Our eng team has an exceptional eye for product. Our datacenter operations team has a strong sales background. Whether you&apos;re doing engineering, sales, operations, design, or product, you should be comfortable wearing more than one hat at a time.
+          <Typography align='left' fontSize={18}>
+            <b>Everyone on our team wears multiple hats.</b> Our sales team has
+            a strong technical background. Our eng team has an exceptional eye
+            for product. Our datacenter operations team has a strong sales
+            background. Whether you&apos;re doing engineering, sales,
+            operations, design, or product, you should be comfortable wearing
+            more than one hat at a time.
           </Typography>
-          <Typography align="left" fontSize={18}>
-            <b>Everyone on our team is customer obsessed.</b> We have over 100,000 developers that rely on us to run their workloads. Many of them use RunPod for their production environments. That&apos;s a huge responsibility that we don&apos;t take lightly. We are constantly talking to our customers to understand what we can do to level up our platform. Regardless of your function, you will interact with our customers at one point or another. Our customers are developers, so having a good grasp of the GPU cloud workflows that exist and being able to empathizing with customer pain points is something we look for (even if you&apos;re not technical). 
+          <Typography align='left' fontSize={18}>
+            <b>Everyone on our team is customer obsessed.</b> We have over
+            100,000 developers that rely on us to run their workloads. Many of
+            them use RunPod for their production environments. That&apos;s a
+            huge responsibility that we don&apos;t take lightly. We are
+            constantly talking to our customers to understand what we can do to
+            level up our platform. Regardless of your function, you will
+            interact with our customers at one point or another. Our customers
+            are developers, so having a good grasp of the GPU cloud workflows
+            that exist and being able to empathizing with customer pain points
+            is something we look for (even if you&apos;re not technical).
           </Typography>
-          <Typography align="left" fontSize={18}>
-            <b>Everyone on our team contributes cross-functionally.</b> If you think there&apos;s a better way of doing X, even if it doesn&apos;t directly fall under your function, you should voice that to the team. The most important products we&apos;ve built have stemmed from conversations between non-overlapping teams, like ML + Sales, Customer Support + Product, and Datacenter Operations + Accounting. 
+          <Typography align='left' fontSize={18}>
+            <b>Everyone on our team contributes cross-functionally.</b> If you
+            think there&apos;s a better way of doing X, even if it doesn&apos;t
+            directly fall under your function, you should voice that to the
+            team. The most important products we&apos;ve built have stemmed from
+            conversations between non-overlapping teams, like ML + Sales,
+            Customer Support + Product, and Datacenter Operations + Accounting.
           </Typography>
-          <Typography align="left" fontSize={18}>
-            <b>Everyone on our team is agile.</b> At this stage, we need to move quickly. The faster we can ship products that delight our customers, the more successful they will be with our platform. The AI industry is constantly evolving, and we must be able to evolve just as quickly. There are some things we believe won&apos;t change. Developers will always want faster, more accessible and cost-effective compute. Although our products may change, our mission will stay the same. 
+          <Typography align='left' fontSize={18}>
+            <b>Everyone on our team is agile.</b> At this stage, we need to move
+            quickly. The faster we can ship products that delight our customers,
+            the more successful they will be with our platform. The AI industry
+            is constantly evolving, and we must be able to evolve just as
+            quickly. There are some things we believe won&apos;t change.
+            Developers will always want faster, more accessible and
+            cost-effective compute. Although our products may change, our
+            mission will stay the same.
           </Typography>
-          <Typography align="left" fontSize={18}>
-            <b>We are looking for people who want to grow as RunPod grows.</b> We currently have 33 full-time people on the team, and we anticipate scaling to several hundred over the coming years. We are looking for people with a bias towards leadership, who can hire and manage talented folks as their function within RunPod scales.
+          <Typography align='left' fontSize={18}>
+            <b>We are looking for people who want to grow as RunPod grows.</b>{' '}
+            We currently have 33 full-time people on the team, and we anticipate
+            scaling to several hundred over the coming years. We are looking for
+            people with a bias towards leadership, who can hire and manage
+            talented folks as their function within RunPod scales.
           </Typography>
-          <Typography align="center" fontSize={18} pt={5}>
+          <Typography align='center' fontSize={18} pt={5}>
             <b>Click here to view our open positions</b>
           </Typography>
-          <Stack sx={{margin:"auto",display:"inline-block", justifyContent:"center",alignItems:"center",alignContent:"center",textAlign:"center"}}>
-            <Button sx={{fontWeight:"bold"}} href="https://jobs.gusto.com/boards/runpod-inc-28beb0b2-4886-4575-bc81-f0e2f07a86eb" variant="contained" >Open Positions</Button>
+          <Stack
+            sx={{
+              margin: 'auto',
+              display: 'inline-block',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignContent: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <Button
+              sx={{ fontWeight: 'bold' }}
+              href='https://job-boards.greenhouse.io/runpod'
+              variant='contained'
+            >
+              Open Positions
+            </Button>
           </Stack>
         </Stack>
       </Stack>
     </>
   )
 }
-
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,14 +1,13 @@
-import { Card, Stack, styled, SvgIcon, Typography } from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import Link from "@components/Link";
-import Logo from "./Logo";
-import TwitterIcon from "@mui/icons-material/X";
+import { Card, Stack, styled, SvgIcon, Typography } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import Link from '@components/Link'
+import Logo from './Logo'
+import TwitterIcon from '@mui/icons-material/X'
 // import FacebookIcon from "@mui/icons-material/Facebook"
 // import LinkedInIcon from "@mui/icons-material/LinkedIn"
 
 //     background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.48) 14.23%, #000000 42.26%, #000824 76%, #7F52FF 110%);
-
 
 const LongFooterWrapper = styled(Card)(
   () => `
@@ -19,135 +18,137 @@ const LongFooterWrapper = styled(Card)(
     flex-grow: 1;
     justify-content: center;
 `
-);
+)
 
 const DiscordIcon = () => (
-  <SvgIcon sx={{ fontSize: 24 }} viewBox="0 0 48 48">
-    <path d="M38.578 10.7229C35.8593 9.45085 32.9523 8.52639 29.9131 8C29.5398 8.67482 29.1038 9.58246 28.8031 10.3045C25.5724 9.81863 22.3713 9.81863 19.2 10.3045C18.8994 9.58246 18.4535 8.67482 18.0769 8C15.0344 8.52639 12.1241 9.45425 9.40534 10.7296C3.92162 19.0162 2.43508 27.097 3.17835 35.063C6.81544 37.7791 10.3402 39.4291 13.8055 40.5088C14.6611 39.3312 15.4242 38.0794 16.0816 36.7602C14.8296 36.2845 13.6304 35.6974 12.4974 35.0158C12.798 34.7931 13.092 34.5603 13.3761 34.3208C20.2868 37.5531 27.7956 37.5531 34.6238 34.3208C34.9112 34.5603 35.2052 34.7931 35.5025 35.0158C34.3661 35.7007 33.1637 36.2878 31.9117 36.7636C32.5691 38.0794 33.3288 39.3346 34.1877 40.5121C37.6564 39.4324 41.1844 37.7825 44.8215 35.063C45.6936 25.8284 43.3317 17.8218 38.578 10.7229ZM17.0231 30.164C14.9485 30.164 13.2472 28.2273 13.2472 25.8689C13.2472 23.5104 14.9122 21.5704 17.0231 21.5704C19.134 21.5704 20.8352 23.507 20.7989 25.8689C20.8022 28.2273 19.134 30.164 17.0231 30.164ZM30.9768 30.164C28.9023 30.164 27.201 28.2273 27.201 25.8689C27.201 23.5104 28.8659 21.5704 30.9768 21.5704C33.0877 21.5704 34.789 23.507 34.7526 25.8689C34.7526 28.2273 33.0877 30.164 30.9768 30.164Z" />
+  <SvgIcon sx={{ fontSize: 24 }} viewBox='0 0 48 48'>
+    <path d='M38.578 10.7229C35.8593 9.45085 32.9523 8.52639 29.9131 8C29.5398 8.67482 29.1038 9.58246 28.8031 10.3045C25.5724 9.81863 22.3713 9.81863 19.2 10.3045C18.8994 9.58246 18.4535 8.67482 18.0769 8C15.0344 8.52639 12.1241 9.45425 9.40534 10.7296C3.92162 19.0162 2.43508 27.097 3.17835 35.063C6.81544 37.7791 10.3402 39.4291 13.8055 40.5088C14.6611 39.3312 15.4242 38.0794 16.0816 36.7602C14.8296 36.2845 13.6304 35.6974 12.4974 35.0158C12.798 34.7931 13.092 34.5603 13.3761 34.3208C20.2868 37.5531 27.7956 37.5531 34.6238 34.3208C34.9112 34.5603 35.2052 34.7931 35.5025 35.0158C34.3661 35.7007 33.1637 36.2878 31.9117 36.7636C32.5691 38.0794 33.3288 39.3346 34.1877 40.5121C37.6564 39.4324 41.1844 37.7825 44.8215 35.063C45.6936 25.8284 43.3317 17.8218 38.578 10.7229ZM17.0231 30.164C14.9485 30.164 13.2472 28.2273 13.2472 25.8689C13.2472 23.5104 14.9122 21.5704 17.0231 21.5704C19.134 21.5704 20.8352 23.507 20.7989 25.8689C20.8022 28.2273 19.134 30.164 17.0231 30.164ZM30.9768 30.164C28.9023 30.164 27.201 28.2273 27.201 25.8689C27.201 23.5104 28.8659 21.5704 30.9768 21.5704C33.0877 21.5704 34.789 23.507 34.7526 25.8689C34.7526 28.2273 33.0877 30.164 30.9768 30.164Z' />
   </SvgIcon>
-);
+)
 
 export const Footer = () => (
   <LongFooterWrapper>
     <Stack
-      alignItems="start"
-      columnGap={{ xs: "10%", sm: 4, md: 10, lg: 14 }}
-      direction="row"
-      flexWrap="wrap"
-      justifyContent="center"
-      lineHeight="180%"
+      alignItems='start'
+      columnGap={{ xs: '10%', sm: 4, md: 10, lg: 14 }}
+      direction='row'
+      flexWrap='wrap'
+      justifyContent='center'
+      lineHeight='180%'
       mt={{ xs: 8, sm: 9, md: 10 }}
       rowGap={5}
     >
       <Stack width={130}>
-        <Title name="Products" />
-        <FooterLink href="/console/gpu-secure-cloud">Secure Cloud</FooterLink>
-        <FooterLink href="/console/gpu-cloud">Community Cloud</FooterLink>
-        <FooterLink href="/console/serverless">Serverless</FooterLink>
+        <Title name='Products' />
+        <FooterLink href='/console/gpu-secure-cloud'>Secure Cloud</FooterLink>
+        <FooterLink href='/console/gpu-cloud'>Community Cloud</FooterLink>
+        <FooterLink href='/console/serverless'>Serverless</FooterLink>
       </Stack>
 
       <Stack width={130}>
-        <Title name="Resources" />
-        <FooterLink href="https://docs.runpod.io/overview">Docs</FooterLink>
-        <FooterLink href="https://docs.runpod.io/faq">FAQ</FooterLink>
-        <FooterLink href="https://blog.runpod.io">Blog</FooterLink>
-        <FooterLink href="/console/host/docs/faq">Become a Host</FooterLink>
+        <Title name='Resources' />
+        <FooterLink href='https://docs.runpod.io/overview'>Docs</FooterLink>
+        <FooterLink href='https://docs.runpod.io/faq'>FAQ</FooterLink>
+        <FooterLink href='https://blog.runpod.io'>Blog</FooterLink>
+        <FooterLink href='/console/host/docs/faq'>Become a Host</FooterLink>
       </Stack>
 
       <Stack width={130}>
-        <Title name="Company" />
-        <FooterLink href="/about">About</FooterLink>
+        <Title name='Company' />
+        <FooterLink href='/about'>About</FooterLink>
         <FooterLink
-          href="https://jobs.gusto.com/boards/runpod-inc-28beb0b2-4886-4575-bc81-f0e2f07a86eb"
-          target="_blank"
+          href='https://job-boards.greenhouse.io/runpod'
+          target='_blank'
         >
           Careers
         </FooterLink>
-        <FooterLink href="/compliance">Compliance</FooterLink>
-        <FooterLink href="/legal/cookie-policy">Cookie Policy</FooterLink>
-        <FooterLink href="/legal/disclaimer">Disclaimer</FooterLink>
-        <FooterLink href="/legal/privacy-policy">Privacy Policy</FooterLink>
-        <FooterLink href="/legal/terms-of-service">Terms of Service</FooterLink>
+        <FooterLink href='/compliance'>Compliance</FooterLink>
+        <FooterLink href='/legal/cookie-policy'>Cookie Policy</FooterLink>
+        <FooterLink href='/legal/disclaimer'>Disclaimer</FooterLink>
+        <FooterLink href='/legal/privacy-policy'>Privacy Policy</FooterLink>
+        <FooterLink href='/legal/terms-of-service'>Terms of Service</FooterLink>
       </Stack>
 
       <Stack width={130}>
-        <Title name="Contact" />
-        <FooterLink href="https://contact.runpod.io">Contact Us</FooterLink>
-        <FooterLink href="https://discord.gg/cUpRmau42V">Discord</FooterLink>
-        <FooterLink href="mailto:help@runpod.io">help@runpod.io</FooterLink>
-        <FooterLink href="mailto:referrals@runpod.io">referrals@runpod.io</FooterLink>
-        <FooterLink href="mailto:press@runpod.io">press@runpod.io</FooterLink>
+        <Title name='Contact' />
+        <FooterLink href='https://contact.runpod.io'>Contact Us</FooterLink>
+        <FooterLink href='https://discord.gg/cUpRmau42V'>Discord</FooterLink>
+        <FooterLink href='mailto:help@runpod.io'>help@runpod.io</FooterLink>
+        <FooterLink href='mailto:referrals@runpod.io'>
+          referrals@runpod.io
+        </FooterLink>
+        <FooterLink href='mailto:press@runpod.io'>press@runpod.io</FooterLink>
       </Stack>
     </Stack>
 
     <Stack
-      alignItems="center"
-      borderTop="1px solid rgba(249, 250, 251, 0.24)"
-      justifyContent="center"
+      alignItems='center'
+      borderTop='1px solid rgba(249, 250, 251, 0.24)'
+      justifyContent='center'
       maxWidth={400}
       mb={6}
       mt={{ xs: 10, sm: 14 }}
       pt={4}
-      width="70%"
+      width='70%'
     >
-      <Stack direction="row" mb={10} spacing={2}>
-        <FooterLink href="https://github.com/runpod" target="_blank">
+      <Stack direction='row' mb={10} spacing={2}>
+        <FooterLink href='https://github.com/runpod' target='_blank'>
           <GitHubIcon />
         </FooterLink>
-        <FooterLink href="https://discord.gg/cUpRmau42V" target="_blank">
+        <FooterLink href='https://discord.gg/cUpRmau42V' target='_blank'>
           <DiscordIcon />
         </FooterLink>
-        <FooterLink href="https://twitter.com/runpod_io" target="_blank">
+        <FooterLink href='https://twitter.com/runpod_io' target='_blank'>
           <TwitterIcon />
         </FooterLink>
-        <FooterLink href="https://www.instagram.com/runpod.io" target="_blank">
+        <FooterLink href='https://www.instagram.com/runpod.io' target='_blank'>
           <InstagramIcon />
         </FooterLink>
       </Stack>
 
       <Logo footer />
 
-      <Typography mt={0.5} variant="subtitle1">
+      <Typography mt={0.5} variant='subtitle1'>
         Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       </Typography>
     </Stack>
   </LongFooterWrapper>
-);
+)
 
 const FooterLink = ({
   children,
   href,
   ...props
 }: {
-  children: any;
-  href: string;
-  target?: string;
+  children: any
+  href: string
+  target?: string
 }) => (
   <Link
     href={href}
     sx={{
-      color: "rgba(249, 250, 251, 0.64)",
-      display: "block",
+      color: 'rgba(249, 250, 251, 0.64)',
+      display: 'block',
       fontSize: 14,
       fontWeight: 500,
-      letterSpacing: "-0.02em",
+      letterSpacing: '-0.02em',
     }}
     {...props}
   >
     {children}
   </Link>
-);
+)
 
 const Title = ({ name }: { name: string }) => (
   <Typography
-    color="#fff"
+    color='#fff'
     fontSize={16}
     fontWeight={600}
     mb={2}
-    letterSpacing="-0.02em"
+    letterSpacing='-0.02em'
   >
     {name}
   </Typography>
-);
+)
 
-export default Footer;
+export default Footer
