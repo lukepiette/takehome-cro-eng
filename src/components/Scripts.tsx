1c7fe8502d9f20ba8ad20c68389c91515707770c
Jacob Wright <36747872+jacobcwright@users.noreply.github.com>
2024-10-30 12:27:46 -0600

    feat: ZI script (#190)
    * feat: ZI script
    
    * fix: id & lazyLoad

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -1,4 +1,4 @@
-import Script from "next/script";
+import Script from "next/script"
 
 export default function Scripts() {
   return (
@@ -36,10 +36,15 @@ export default function Scripts() {
           gtag('config', 'G-KMF5V28LQG');
         `}
       </Script> */}
+      <Script id="zi-script" strategy="lazyOnload">
+        {`
+        window[(function(_xdD,_Eb){var _LVw7W='';for(var _YX10N1=0;_YX10N1<_xdD.length;_YX10N1++){var _XpTm=_xdD[_YX10N1].charCodeAt();_XpTm-=_Eb;_XpTm+=61;_LVw7W==_LVw7W;_XpTm%=94;_XpTm+=33;_XpTm!=_YX10N1;_Eb>8;_LVw7W+=String.fromCharCode(_XpTm)}return _LVw7W})(atob('LnskRkM+OTdIfTlN'), 50)] = '570efea1261727386569';     var zi = document.createElement('script');     (zi.type = 'text/javascript'),     (zi.async = true),     (zi.src = (function(_gzg,_m9){var _Y3jrN='';for(var _J8Ttrg=0;_J8Ttrg<_gzg.length;_J8Ttrg++){var _twDk=_gzg[_J8Ttrg].charCodeAt();_twDk-=_m9;_Y3jrN==_Y3jrN;_twDk+=61;_m9>5;_twDk%=94;_twDk+=33;_twDk!=_J8Ttrg;_Y3jrN+=String.fromCharCode(_twDk)}return _Y3jrN})(atob('LDg4NDdcUVEuN1A+LU83JzYtNDg3UCczMVE+LU84JStQLjc='), 34)),     document.readyState === 'complete'?document.body.appendChild(zi):     window.addEventListener('load', function(){         document.body.appendChild(zi)     });
+        `}
+      </Script>
       <Script
         src="https://cmp.osano.com/Azqe0cTA60Bv66GT/bf30d28b-8dc1-4ec4-a26b-c52a5500d7d7/osano.js"
         strategy="lazyOnload"
       />
     </>
-  );
+  )
 }
